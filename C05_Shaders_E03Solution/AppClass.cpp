a695e397ac4575e0827938d3d3f9449c9df63286
dbg9180 <70482975+dbg9180@users.noreply.github.com>
2023-02-01 16:38:35 -0500

    E03 done

--- C05_Shaders_E03Solution/AppClass.cpp
+++ C05_Shaders_E03Solution/AppClass.cpp
@@ -67,7 +67,7 @@ void AppClass::InitOpenGL(void)
 }
 void AppClass::InitShaders(void)
 {
-	m_uShaderProgramID = LoadShaders("Shaders//BasicColor.vs", "Shaders//BasicColor.fs");
+	m_uShaderProgramID = LoadShaders("Shaders//BasicColor.vs", "Shaders//BasicColorE03.fs");
 	glUseProgram(m_uShaderProgramID);
 }
 void AppClass::InitVariables(void)
--- _Binary/Shaders/BasicColor.fs
+++ _Binary/Shaders/BasicColor.fs
@@ -3,15 +3,22 @@
 in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
+uniform int ShouldInvert;
 
 out vec4 Fragment;
 
 void main()
 {
+	vec3 output = Color;
 	Fragment = vec4(Color,1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
-		Fragment = vec4(SolidColor, 1);
+		output = SolidColor;
+
+	else if(ShouldInvert == 1){
+		output = vec3(1.0f,1.0f,1,0f)-output;
+	}
+	Fragment= vec4(output,1);
 
 	return;
 }
